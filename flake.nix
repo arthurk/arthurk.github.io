428f494fbc321dc07386fa1615b8f2ec674c1660
Arthur Koziel <arthur@arthurkoziel.com>
2023-10-10 21:18:20 +0200

    update

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake for personal website";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zola
+          ];
+        };
+      }
+    );
+}
