04188a6affbcd370e5c47256e6a58c0f995a2f2d
Arthur Koziel <arthur@arthurkoziel.com>
2023-11-22 17:14:59 +0100

    update

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
           buildInputs = with pkgs; [
             zola
             nil
+            just
           ];
         };
       }
---
+++ justfile
@@ -0,0 +1,4 @@
+serve:
+	zola -r zola-project serve --drafts
+build:
+	zola -r zola-project build --drafts
